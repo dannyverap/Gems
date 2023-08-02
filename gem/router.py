1619df1df60ca287b022f17cd6a26e5aa1e37b47
dannyverap <danny.verap@gmail.com>
2023-08-02 09:58:11 -0500

    commit 1

--- gem/router.py
+++ gem/router.py
@@ -24,7 +24,14 @@ def get_gem_by_id(*,db: Session = Depends(get_db) ,gem_id:int):
     gem = service_get_by_id(db, id=gem_id)
     return gem
 
-
+@gem_router.patch("/{gem_id}", response_model=GemWithProperties,status_code=status.HTTP_200_OK)
+def update_gem(*,db:Session=Depends(get_db), gem_id:int, gem:GemUpdate):
+    db_gem = service_get_by_id(db,id=gem_id)
+    if not db_gem:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Gem not found")
+    
+    updated_gem = service_update_gem(db,gem,db_gem)
+    return updated_gem
 
 # @gem_router.post("/",response_model=GemRead,status_code=status.HTTP_201_CREATED)
 # def create_gem(*, session: Session = Depends(get_session), gem:GemCreate):
--- gem/service.py
+++ gem/service.py
@@ -12,3 +12,12 @@ def service_create(db: Session, gem: GemCreate):
 
 def service_get_by_id(db:Session,id:int):
     return db.exec(select(Gem).where(Gem.id == id)).first()
+
+def service_update_gem(db:Session, gem: GemUpdate,db_gem:Gem):
+    gem_data = gem.dict(exclude_unset=True)
+    for key,value in gem_data.items():
+        setattr(db_gem, key,value)
+    db.add(db_gem)
+    db.commit()
+    db.refresh(db_gem)
+    return db_gem
